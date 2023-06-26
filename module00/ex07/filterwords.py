38111d5334142fde34b9c16bcfa438ac9eb0f643
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-06-26 17:39:53 +0200

    ex06 fini

--- module00/ex07/filterwords.py
+++ module00/ex07/filterwords.py
@@ -6,9 +6,37 @@ def atoi(str):
 		res = res * 10 + (ord(str[i]) - ord('0'))
 	return res
 
-if len(sys.argv) != 3:
-    print("ERROR")
-    exit()
-if not sys.argv[2].isdigit():
-    print("ERROR")
-    exit()
+def haspuncMark(str):
+	for i in [0, len(str) - 1]:
+		if str[i] == ',' or str[i] == '?' or str[i] == ';' or str[i] == '.' or str[i] == ':' or str[i] == '!':
+			return True
+	return False 
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print("ERROR")
+		exit()
+	if not sys.argv[2].isdigit():
+		print("ERROR")
+		exit()
+	S = sys.argv[1]
+	N = atoi(sys.argv[2])
+
+	list = S.split()
+
+	for x in list:
+		if haspuncMark(x):
+			list.remove(x)
+
+	print(list)
+	print(len(list))
+
+	# for x in list:
+	# 	print(len(x))
+	# 	if len(x) < N:
+	# 		list.remove(x)
+	
+	print(list)
+	to_print = """S = {}
+N = {}"""
+	print(to_print.format(S, N))
