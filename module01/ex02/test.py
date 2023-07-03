4e6bc1a46788d1192d4d130f9d1ba02ee04cec26
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-07-03 10:55:24 +0200

    module 01 ex02 j'arrive pas avec __repr__

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -1,1 +1,126 @@
+from vector import Vector
+
 if __name__ == "__main__":
+	valuesV1 = [[1.], [2.], [3.]]
+
+	valuesU1 = [[1., 2., 3.]]
+    
+	# print(valuesU1[0])
+
+	v1 = Vector(valuesV1)
+	u1 = Vector(valuesU1)
+
+	# print(u1)
+	# # print(u1.dot(u1))
+
+	# # print(f"v1 = {v1} et T(v1) = {v1.T()}")
+	# print(f"u1 = {u1} et T(u1) = {u1.T().values}")
+
+	# Column vector of shape n * 1
+	v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+	# v2 = v1 + v1
+	# print(f"v1 + v1 = {v2}")
+	# v2 = v2 - v1
+	# print(f"v2 - v1 = {v2}")
+	# v2 = v1 * 5
+	# print(f"v1 * 5 = {v2}")
+	# v2 = 5 * v1
+	# print(f"5 * v1 = {v2}")
+	# # Expected output:
+	# # Vector([[0.0], [5.0], [10.0], [15.0]])
+	
+	# Row vector of shape 1 * n
+	u1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+	# u2 = u1 + u1
+	# print(f"u1 + u1 = {u2}")
+	# u2 = u2 - u1
+	# print(f"u2 - u1 = {u2}")
+	# u2 = u1 * 5
+	# print(f"u1 * 5 = {u2}")
+	# u2 = 5 * u1
+	# print(f"5 * u1 = {u2}")
+	# # Expected output
+	# # Vector([[0.0, 5.0, 10.0, 15.0]])
+	
+	# v2 = v1 / 2.0
+	# print(f"v1 / 2 = {v2}")
+	# # Expected output
+	# # Vector([[0.0], [0.5], [1.0], [1.5]])
+	
+	# u2 = u1 / 2.0
+	# print(f"u1 / 2 = {u2}")
+
+	# v1 / 0.0
+	# # Expected ouput
+	# # ZeroDivisionError: division by zero.
+	
+	# 2.0 / v1
+	# # Expected output:
+	# # NotImplementedError: Division of a scalar by a Vector is not defined here.
+
+	# Column vector of shape (n, 1)
+	# print(Vector([[0.0], [1.0], [2.0], [3.0]]).shape)
+	# Expected output
+	# (4,1)
+
+	# print(Vector([[0.0], [1.0], [2.0], [3.0]]).values)
+	# Expected output
+	# [[0.0], [1.0], [2.0], [3.0]]
+
+	# Row vector of shape (1, n)
+	# print(Vector([[0.0, 1.0, 2.0, 3.0]]).shape)
+	# Expected output
+	# (1,4)
+
+	# print(Vector([[0.0, 1.0, 2.0, 3.0]]).values)
+	# Expected output
+	# [[0.0, 1.0, 2.0, 3.0]]
+
+	# Example 1:
+	v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+	# print(v1.shape)
+	# Expected output:
+	# (4,1)
+
+	# print(v1.T())
+	# Expected output:
+	# Vector([[0.0, 1.0, 2.0, 3.0]])
+	
+	# print(v1.T().shape)
+	# Expected output:
+	# (1,4)
+	
+	# Example 2:
+	v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
+	# print(v2.shape)
+	# Expected output:
+	# (1,4)
+	
+	# print(v2.T())
+	# Expected output:
+	# Vector([[0.0], [1.0], [2.0], [3.0]])
+	
+	# print(v2.T().shape)
+	# Expected output:
+	# (4,1)
+	
+	# Example 1:
+	v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+	# v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+	# print(v1.dot(v2))
+	# Expected output:
+	# 18.0
+	
+	# v3 = Vector([[1.0, 3.0]])
+	# v4 = Vector([[2.0, 4.0]])
+	# print(v3.dot(v4))
+	# Expected output:
+	# 13.0
+	
+	v1
+	# Expected output: to see what __repr__() should do
+	# [[0.0, 1.0, 2.0, 3.0]]
+	
+	# print(v1)
+	# Expected output: to see what __str__() should do
+	# [[0.0, 1.0, 2.0, 3.0]]
--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -1,51 +1,162 @@
 class Vector:
-	def __init__(self, values, shape):
+	def __init__(self, values):
 		#checks
 		assert isinstance(values, list), "values: list of list of floats (for row vector) or list of lists of single float (for column vector)"
-		assert len(values) == 1, "values, len : 1"
-		if (len(values[0]) != 1):#[1.], [2.], [3.]
-			for i in range(len(values[0])):
-				assert isinstance(values[0][i], list), "the list must contain lists of 1 float"
-				assert len(values[0][i]) == 1, "the list must contain lists of 1 float"
-				assert isinstance(values[0][i][0], float), "the list must contain lists of 1 float"
-		else:#[1., 2., 3.]
+		if (len(values) != 1):#case column
+			for i in range(len(values)):
+				assert isinstance(values[i], list), "the list must contain lists of 1 float or 1 list of several floats"
+				assert len(values[i]) == 1, "the list must contain lists of 1 float"
+				assert (isinstance(values[i][0], float) or isinstance(values[i][0], int)), "the list must contain lists of 1 float"
+		else:#case line
 			assert isinstance(values[0], list), "the list must contain 1 list of floats"
 			for i in range(len(values[0])):
-				assert isinstance(values[0][i], float), "the list must contain 1 list of floats"
-		assert isinstance(shape, tuple), "shape must be a uple of 2 int"
-		assert len(shape) == 2, "shape must be a uple of 2 int"
-		assert isinstance(shape[0], int), "shape must be a uple of 2 int"
-		assert isinstance(shape[1], int), "shape must be a uple of 2 int"
-		if (len(values[0]) != 1):
-			assert shape[0] == len(values[0]), "shape must be (n, 1) where n is the size of the vector"
-			assert shape[1] == 1, "shape must be (n, 1)"
-		else:
-			assert shape[0] == 1, "shape must be (1, n)"
-			assert shape[1] == len(values[0]), "shape must be (1, n) where n is the size of the vector"
+				assert (isinstance(values[0][i], float) or isinstance(values[0][i], int)), "the list must contain 1 list of floats"
 		
 		#init
 		self.values = values
-		self.shape = shape
+		self.shape = [0, 0]
+		if (len(values) != 1):#case column
+			self.shape[0] = len(values)
+			self.shape[1] = 1
+		else:#case line
+			self.shape[0] = 1
+			self.shape[1] = len(values[0])
+		self.shape = tuple(self.shape)
 
+	#overload
+	def __str__(self) -> str:
+		to_print = "["
+		if self.shape[0] != 1:#case column
+			for i in range(len(self.values)):
+				to_print += f"[{self.values[i][0]}]"
+				if i != len(self.values) - 1:
+					to_print += ", "
+			to_print += "]"
+		else:#case line
+			to_print += "["
+			for i in range(len(self.values[0])):
+				to_print += f"{self.values[0][i]}"
+				if i != len(self.values[0]) - 1:
+					to_print += ", "
+			to_print += "]]"
+		return to_print
+	
+	def __repr__(self) -> str:
+		to_print = "["
+		if self.shape[0] != 1:#case column
+			for i in range(len(self.values)):
+				to_print += f"[{self.values[i][0]}]"
+				if i != len(self.values) - 1:
+					to_print += ", "
+			to_print += "]"
+		else:#case line
+			to_print += "["
+			for i in range(len(self.values[0])):
+				to_print += f"{self.values[0][i]}"
+				if i != len(self.values[0]) - 1:
+					to_print += ", "
+			to_print += "]]"
+		return to_print
+		# return str(self)
+
+	def __add__(self, v):
+		#check
+		assert self.shape == v.shape, "this is between 2 vectors of same shape"
+		#prog
+		values = []
+		if self.shape[0] != 1:#case column
+			for i in range(len(self.values)):
+				values.append([self.values[i][0] + v.values[i][0]])
+		else:#case line
+			for i in range(len(self.values[0])):
+				values.append(self.values[0][i] + v.values[0][i])
+			values = [values]
+		return Vector(values)
+	
+	def __sub__(self, v):
+		#check
+		assert self.shape == v.shape, "this is between 2 vectors of same shape"
+		#prog
+		values = []
+		if self.shape[0] != 1:#case column
+			for i in range(len(self.values)):
+				values.append([self.values[i][0] - v.values[i][0]])
+		else:#case line
+			for i in range(len(self.values[0])):
+				values.append(self.values[0][i] - v.values[0][i])
+			values = [values]
+		return Vector(values)
+	
+	def __mul__(self, l):
+		assert (isinstance(l, float) or isinstance(l, int)), "this is between a vector and a scalar"
+		values = []
+		if self.shape[0] != 1:#case column
+			for i in range(len(self.values)):
+				values.append([self.values[i][0] * l])
+		else:#case line
+			for i in range(len(self.values[0])):
+				values.append(self.values[0][i] * l)
+			values = [values]
+		return Vector(values)
+	
+	def __rmul__(self, l):
+		assert (isinstance(l, float) or isinstance(l, int)), "this is between a vector and a scalar"
+		values = []
+		if self.shape[0] != 1:#case column
+			for i in range(len(self.values)):
+				values.append([self.values[i][0] * l])
+		else:#case line
+			for i in range(len(self.values[0])):
+				values.append(self.values[0][i] * l)
+			values = [values]
+		return Vector(values)
+	
+	def __truediv__(self, l):
+		assert (isinstance(l, float) or isinstance(l, int)), "this is between a vector and a scalar"
+		values = []
+		if (l == 0):
+			try:
+				self.values[0][0] / 0
+			except ZeroDivisionError:
+				raise ZeroDivisionError("division by zero")
+		if self.shape[0] != 1:#case column
+			for i in range(len(self.values)):
+				values.append([self.values[i][0] / l])
+		else:#case line
+			for i in range(len(self.values[0])):
+				values.append(self.values[0][i] / l)
+			values = [values]
+		return Vector(values)
+	
+	def __rtruediv__(self, l):
+		assert (isinstance(l, float) or isinstance(l, int)), "this is between a vector and a scalar"
+		try :
+			l / [self.values[0][0]]
+		except NotImplementedError:
+			raise NotImplementedError("Division of a scalar by a Vector is not defined here.")
+	
 	#methods
 	def dot(self, v):
 		#checks
-		assert isinstance(v, Vector), "this is between 2 vectors od same shape"
-		assert self.shape == v.shape, "this is between 2 vectors od same shape"
+		assert isinstance(v, Vector), "this is between 2 vectors of same shape"
+		assert self.shape == v.shape, "this is between 2 vectors of same shape"
 		#prog
 		res = 0.
-		for i in range(len(v.values[0])):
-			if v.shape[1] == 1:
-				res += self.values[0][i][0] * v.values[0][i][0]
-			else:
-				res += self.values[0][i] * v.values[0].[i]
+		if v.shape[1] == 1:#case column
+			for i in range(len(v.values)):
+				res += self.values[i][0] * v.values[i][0]
+		else:#case line
+			for i in range(len(self.values[0])):
+				res += self.values[0][i] * v.values[0][i]
 		return res
 	
 	def T(self):
-		res = []
-		if self.shape[1] == 1:
-			res[0] = []
-			#...
-		else:
+		values = []
+		if self.shape[1] == 1:#case column -> line
+			for i in range(len(self.values)):
+				values.append(self.values[i][0])
+			values = [values]
+		else:#case line -> column
 			for i in range(len(self.values[0])):
-				#...
+				values.append([self.values[0][i]])
+		return Vector(values)
