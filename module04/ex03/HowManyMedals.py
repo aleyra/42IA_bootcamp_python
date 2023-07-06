fd2eb72387daf7145287f3ea341eecc5867e04b6
lburnet <lburnet@ubuntu.myguest.virtualbox.org>
2023-07-06 17:27:54 +0200

    module 04 ok fiouh

--- module04/ex03/HowManyMedals.py
+++ module04/ex03/HowManyMedals.py
@@ -7,14 +7,24 @@ def how_many_medals(df, name):
 	# print(df.columns)
 	tmp = df.groupby(df.Name)
 	tdf = tmp.get_group(name)
+	# print(tdf)
 	year = pd.unique(tdf[['Year']].values.ravel())
 	# print(type(year))
-	for i in range(len(year)):
-		print(year[i])
-		gold = 0
-		silver = 0
-		bronze = 0
+	tmp = tdf.groupby(tdf.Year)
 
+	for i in year:
+		tdic = dict({'G':0, 'S':0, 'B':0})
+		y_i = tmp.get_group(i)
+		m = y_i.groupby(y_i.Medal)
+		type_medal = pd.unique(y_i[['Medal']].values.ravel())
+		if 'Gold' in type_medal:
+			tdic['G'] = m.get_group('Gold').shape[0]
+		if 'Silver' in type_medal:
+			tdic['S'] = m.get_group('Silver').shape[0]
+		if 'Bronze' in type_medal:
+			tdic['B'] = m.get_group('Bronze').shape[0]
+		res.update({i:tdic})
+	print(res)
     
 if __name__ == "__main__":
 	loader = FileLoader()
